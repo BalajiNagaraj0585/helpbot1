562fe51a8b175dfae11193cbf6f75ce5779c126e
BalajiNagaraj0585 <balaji.nagaraj@accenture.com>
2017-04-07 12:20:00 +0530

    package and app files - 07-apr-2017 - BN

---
+++ app.js
@@ -0,0 +1,21 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: 'c6db87fd-b092-43f6-8652-b84219a75f4d', appPassword: 'nMpHqvJODYbjdHeF3o7sTJu' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
